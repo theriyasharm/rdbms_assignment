2e9fec4687338a3d5d82ba91fa072a592369368b
theriyasharm <122430050+theriyasharm@users.noreply.github.com>
2024-05-18 12:12:25 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost', 
+    user: 'root', 
+    password: 'password', 
+    database: 'cake' 
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+module.exports = connection;
